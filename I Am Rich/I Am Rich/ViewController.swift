39a6715c71b408e4e3b4aa6c5855c5f1f4f66f47
Nadiar AS <codxse@gmail.com>
2020-01-25 22:34:05 +0700

    kBOOM!

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Nadiar Ahmad Syaripul on 12/01/20.
+//  Copyright © 2020 Nadiar Ahmad Syaripul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
