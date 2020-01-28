1de9716acf1b8ea6140280229a21bbb373d47452
Nadiar AS <codxse@gmail.com>
2020-01-28 09:29:51 +0700

    section 4

--- Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController { 
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var leftDiceIndex = 0
+    
+    var rightDiceIndex = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // diceImageViewOne.image = #imageLiteral(resourceName: "DiceSix")
+        // diceImageViewOne.alpha = 0.5
+        // diceImageViewTwo.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        // print("Jabaja funky night.")
+        
+        // diceImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
+        // diceImageViewTwo.image = #imageLiteral(resourceName: "DiceFour")
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        
+        diceImageViewTwo.image = diceArray.randomElement()
+        
+        if leftDiceIndex < 5 {
+            leftDiceIndex += 1
+        }
+        
+        if rightDiceIndex > 0 {
+            rightDiceIndex -= 1
+        }
+    }
+    
 }
 
